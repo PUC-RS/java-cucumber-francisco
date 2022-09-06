732fb942305dc064c44cfb6f31bb6618e912ae22
Francisco Joao Lucca Neto <franciscojoaoluccaneto@gmail.com>
2022-09-06 11:26:30 -0300

    Enable workflow

--- src/main/java/br/masmangan/beecrowd/bee1059/Main.java
+++ src/main/java/br/masmangan/beecrowd/bee1059/Main.java
@@ -3,7 +3,6 @@ package br.masmangan.beecrowd.bee1059;
 public final class Main {
     private Main() {
     }
-
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
