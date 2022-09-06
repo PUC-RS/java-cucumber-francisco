2ae6d5e67d4fa821127ac71617e8e3ece7b290be
Francisco Joao Lucca Neto <franciscojoaoluccaneto@gmail.com>
2022-09-06 09:19:02 -0300

    Adding automation and what its needed

---
+++ src/main/java/br/masmangan/beecrowd/bee1059/Main.java
@@ -0,0 +1,16 @@
+package br.masmangan.beecrowd.bee1059;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
---
+++ src/test/java/br/masmangan/beecrowd/bee1059/Bee1059Steps.java
@@ -0,0 +1,38 @@
+package br.masmangan.beecrowd.bee1059;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class Bee1059Steps {
+    private String actual;
+
+    @When("Program Runs")
+    public void program_runs() {
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream outputStream = new PrintStream(byteArrayOutputStream);
+
+        PrintStream previousOut = System.out;
+
+        System.setOut(outputStream);
+
+        Main.main(null);
+
+        actual = byteArrayOutputStream.toString();
+
+        outputStream.close();
+
+        System.setOut(previousOut);
+    }
+
+    @Then("output should be")
+    @Then("a saída deve ser")
+    public void output_should_be(String expected) {
+        assertEquals(expected, actual);
+    }
+}
