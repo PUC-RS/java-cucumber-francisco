b288c7ab86a46b4c1ae50d73c5ab38c4d829b778
Francisco Joao <francisco.joao.lucca1@hp.com>
2022-09-06 20:04:06 -0300

    fix step/test/main

--- src/main/java/br/masmangan/beecrowd/bee1059/Main.java
+++ src/main/java/br/masmangan/beecrowd/bee1059/Main.java
@@ -1,12 +1,15 @@
 package br.masmangan.beecrowd.bee1059;
 
+import static java.lang.System.out;
+
 public final class Main {
     private Main() {
     }
     public static void main(String[] args) {
+        final StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
-                System.out.println(i);
+                out.print(i + "\n");
             }
         }
     }
